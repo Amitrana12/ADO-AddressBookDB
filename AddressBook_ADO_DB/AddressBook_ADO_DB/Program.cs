f87cc40704deb7caed5f7b6a73b7118dd18d16b6
amit <[amirana14325@gmail.com]>
2021-04-17 22:11:37 +0530

    [Amit Rana]Added::weclome massage

---
+++ AddressBook_ADO_DB/AddressBook_ADO_DB/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AddressBook_ADO_DB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to my Addrss Book with ADo.Net and sql server by Amit Rana");
+        }
+    }
+}
